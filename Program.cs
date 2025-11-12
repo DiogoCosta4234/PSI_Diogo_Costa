ec7cf3aa009fa19f399396806d360a7926e01bd0
Diogo Costa <al.916492@aeaav.pt>
2025-11-12 17:11:23 +0000

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+class Program
+{
+    static void checkAge(int age)
+    {
+        if (age < 18)
+        {
+            Console.WriteLine("Acesso negado - Menor de Idade");
+        }
+        else
+        {
+            Console.WriteLine("Acesso concedido - Maior de Idade");
+        }
+    }
+    static void Main(string[] args)
+    {
+      
+        checkAge(27);
+        Console.ReadLine();
+    }
+}
